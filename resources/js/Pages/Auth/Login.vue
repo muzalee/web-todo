d2d25ab9de851edaf092689adcca703ad0448ef4
muza <muzallyefah@gmail.com>
2023-12-19 19:14:52 +0800

    feat: form validation

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -7,24 +7,51 @@
         <div class="my-6 text-xl">
             TODO WEB APP
         </div>
-        <form action="" class="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
-            <div class="pb-2 pt-4">
-                <input class="block w-full p-4 text-lg rounded-lg bg-black" type="email" name="email" id="email" placeholder="Email" required>
-            </div>
-            <div class="pb-2 pt-4">
-                <input class="block w-full p-4 text-lg rounded-lg bg-black" type="password" name="password" id="password" placeholder="Password" required>
-            </div>
-            <div class="text-right text-gray-400 hover:underline hover:text-gray-100">
-                <a href="#">Forgot your password?</a>
-            </div>
-            <div class="pb-2 pt-4 mt-5">
-                <button class="uppercase block w-full p-4 text-lg rounded-xl bg-indigo-500 hover:bg-indigo-600 focus:outline-none">sign in</button>
-            </div>
+        <form @submit.prevent="login" class="sm:w-2/3 w-full px-4 lg:px-0 mx-auto" novalidate>
+        <div class="pb-2 pt-4">
+            <input class="block w-full p-4 text-lg rounded-lg bg-black" type="email" name="email" placeholder="Email" v-model="userEmail">
+            <div v-if="v$.userEmail.$error" class="text-red-500 text-sm mt-2 text-left">{{ v$.userEmail.$errors[0].$message }}</div>
+        </div>
+        <div class="pb-2 pt-4">
+            <input class="block w-full p-4 text-lg rounded-lg bg-black" type="password" name="password" placeholder="Password" v-model="userPassword">
+            <Field name="password" v-model="userPassword" />
+            <div v-if="v$.userPassword.$error" class="text-red-500 text-sm mt-2 text-left">{{ v$.userPassword.$errors[0].$message }}</div>
+        </div>
+        <div class="text-right text-gray-400 hover:underline hover:text-gray-100">
+            <a href="#">Forgot your password?</a>
+        </div>
+        <div class="pb-2 pt-4 mt-5">
+            <button class="uppercase block w-full p-4 text-lg rounded-xl bg-indigo-500 hover:bg-indigo-600 focus:outline-none">sign in</button>
+        </div>
         </form>
     </AuthLayout>
-</template>
+ </template>
 
-<script setup lang="ts">
-import AppHead from "@/components/AppHead.vue";
-import AuthLayout from "@/Layouts/AuthLayout.vue";
-</script>
+ <script setup lang="ts">
+ import { ref } from 'vue';
+ import AppHead from "@/components/AppHead.vue";
+ import AuthLayout from "@/Layouts/AuthLayout.vue";
+ import { useVuelidate } from '@vuelidate/core'
+ import { required, email } from '@vuelidate/validators'
+
+ const userEmail = ref('');
+ const userPassword = ref('');
+
+ const rules = {
+  userEmail: { required, email },
+  userPassword: { required }
+ }
+
+ const v$ = useVuelidate(rules, { userEmail, userPassword })
+
+ const login = () => {
+    v$.value.$touch();
+    if (v$.value.$invalid) {
+        return;
+    }
+    alert(JSON.stringify({ email: userEmail.value, password: userPassword.value }, null, 2));
+    userEmail.value = '';
+    userPassword.value = '';
+    v$.value.$reset();
+ };
+ </script>
